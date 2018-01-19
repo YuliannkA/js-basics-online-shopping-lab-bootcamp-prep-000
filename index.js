452407e9112261b62912b85c93525ed8654c7ecf
Yulia Drozdova <yuliannka@gmail.com>
2018-01-19 23:57:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,7 +63,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  
-  
+  if (cardNumber === ()) {
+  console.log(`Sorry, we don't have a credit card on file for you.`)
+  } else {
+    
+  }
   
 }
