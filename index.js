4892eb335cf93089f48ed5a1de431dd7c2a78a3d
Yulia Drozdova <yuliannka@gmail.com>
2018-01-19 23:38:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,10 +54,10 @@ function total() {
 function removeFromCart(item) {
   
   for ( var i=0; i < cart.length; i++) {
-    if (cart[i].hasOwnPrpperty(item)) {
+    if (cart[i].hasOwnProperty(item)) {
           delete cart[i];
  //       } else {
-            return cart
+          return cart
  //       }
       }
   }
